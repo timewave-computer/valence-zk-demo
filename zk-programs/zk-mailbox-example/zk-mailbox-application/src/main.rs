fd45bc2d46add0717f678249fd16e489cd40447f
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2025-04-29 21:02:19 +0200

    verify state roots for ethereum and tendermint in mailbox example

--- zk-programs/zk-mailbox-example/zk-mailbox-application/src/main.rs
+++ zk-programs/zk-mailbox-example/zk-mailbox-application/src/main.rs
@@ -1,5 +1,6 @@
 #![no_main]
 
+use ssz_merkleize::merkleize::{self, merkleize_keys, uint64_to_le_256};
 use types::{
     MailboxApplicationCircuitInputs, MailboxApplicationCircuitOutputs,
     deserialize_ethereum_proof_value_as_string, deserialize_neutron_proof_value_as_string,
@@ -10,4 +11,27 @@ sp1_zkvm::entrypoint!(main);
 fn main() {
     let inputs: MailboxApplicationCircuitInputs = serde_json::from_slice::<MailboxApplicationCircuitInputs>(&sp1_zkvm::io::read_vec())
         .expect("Failed to deserialize MailboxApplicationCircuitInputs");
+    let tendermint_header_hash = inputs.neutron_block_header.hash().as_bytes().to_vec();
+    // verify the neutron app hash against the header root
+    assert_eq!(tendermint_header_hash, inputs.neutron_root_opening.data);
+    let target_header_root = merkleize_keys(vec![
+        uint64_to_le_256(inputs.beacon_block_header.slot.parse::<u64>().unwrap()),
+        uint64_to_le_256(inputs.beacon_block_header.proposer_index.parse::<u64>().unwrap()),
+        hex::decode(&inputs.beacon_block_header.parent_root.trim_start_matches("0x"))
+        .unwrap()
+        .to_vec(),
+        hex::decode(&inputs.beacon_block_header.state_root.trim_start_matches("0x"))
+            .unwrap()
+            .to_vec(),
+        hex::decode(&inputs.beacon_block_header.body_root.trim_start_matches("0x"))
+            .unwrap()
+            .to_vec(),
+    ]);
+    // the ethereum state root against which we verify our storage proofs
+    let ethereum_state_root = hex::decode(&inputs.beacon_block_header.state_root.trim_start_matches("0x"))
+    .unwrap();
+    
+    // the neutron app hash against which we verify our storage proofs
+    let tendermint_app_hash = inputs.neutron_block_header.app_hash.as_bytes().to_vec();
+    assert_eq!(target_header_root, inputs.ethereum_root_opening.data);
 }
