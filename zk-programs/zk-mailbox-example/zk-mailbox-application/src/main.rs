e0878ec137d22d802b95a1763fa7c9050d3fd66e
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2025-04-29 21:08:16 +0200

    verify neutron and ethereum storage proofs in mailbox example

--- zk-programs/zk-mailbox-example/zk-mailbox-application/src/main.rs
+++ zk-programs/zk-mailbox-example/zk-mailbox-application/src/main.rs
@@ -6,6 +6,7 @@ use types::{
     deserialize_ethereum_proof_value_as_string, deserialize_neutron_proof_value_as_string,
 };
 use valence_coprocessor_core::MemorySmt;
+use common_merkle_proofs::merkle::types::MerkleVerifiable;
 
 sp1_zkvm::entrypoint!(main);
 fn main() {
@@ -32,6 +33,27 @@ fn main() {
     .unwrap();
     
     // the neutron app hash against which we verify our storage proofs
-    let tendermint_app_hash = inputs.neutron_block_header.app_hash.as_bytes().to_vec();
+    let neutron_app_hash = inputs.neutron_block_header.app_hash.as_bytes().to_vec();
+
+    // verify the ethereum storage proofs
+    for ethereum_proof in inputs.ethereum_storage_proofs {
+        ethereum_proof
+            .1
+            .verify(&ethereum_proof.2)
+            .expect("Failed to verify Ethereum storage proof");
+        ethereum_proof
+            .0
+            .verify(&ethereum_state_root)
+            .expect("Failed to verify Ethereum account proof");
+    };
+
+    // verify the neutron storage proofs
+    for neutron_proof in inputs.neutron_storage_proofs {
+        // verify the proof against the neutron root
+        neutron_proof
+            .verify(&neutron_app_hash)
+            .expect("Failed to verify Neutron storage proof");
+    };
+
     assert_eq!(target_header_root, inputs.ethereum_root_opening.data);
 }
